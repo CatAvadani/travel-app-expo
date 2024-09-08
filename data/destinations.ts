f7963df2c431e5ebd2025987d2c55e6a764727bb
CatAvadani <catalinaavadani@gmail.com>
2024-09-08 16:52:33 +0200

    Update destination categories and add new destinations

--- data/destinations.ts
+++ data/destinations.ts
@@ -155,4 +155,199 @@ export const destinationsList = [
     location: 'Maasai Mara, Kenya',
     category: 'Tours',
   },
+  {
+    id: 13,
+    name: 'Lakeside Haven Cabin Retreat',
+    image:
+      'https://images.pexels.com/photos/534164/pexels-photo-534164.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'A tranquil lakeside cabin perfect for those seeking a peaceful escape from city life. Enjoy fishing, kayaking, and stargazing at night.',
+    rating: 4.5,
+    price: '200',
+    duration: '3',
+    location: 'Lake, UK',
+    category: 'Parks',
+  },
+  {
+    id: 14,
+    name: 'Park Adventure Day',
+    image:
+      'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    description:
+      'A sprawling park featuring lush meadows, walking trails, and picnic areas. Perfect for a day out with family and friends.',
+    rating: 4.3,
+    price: 'Free',
+    duration: '1 ',
+    location: 'Seattle, USA',
+    category: 'Parks',
+  },
+  {
+    id: 15,
+    name: 'National Park',
+    image:
+      'https://images.pexels.com/photos/612999/pexels-photo-612999.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'A stunning national park where you can hike up to watch the sunrise over majestic mountains. A perfect getaway for nature lovers.',
+    rating: 4.8,
+    price: '100',
+    duration: '2 ',
+    location: 'Banff, Canada',
+    category: 'Parks',
+  },
+  {
+    id: 16,
+    name: 'Mountain Stream Camping',
+    image:
+      'https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Set up camp next to a peaceful mountain stream and enjoy the fresh air and nature’s tranquility. Ideal for hiking and fishing.',
+    rating: 4.7,
+    price: '50',
+    duration: '3',
+    location: 'Rocky Mountains, USA',
+    category: 'Camping',
+  },
+  {
+    id: 17,
+    name: 'Camping Oasis',
+    image:
+      'https://images.pexels.com/photos/2873086/pexels-photo-2873086.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Escape to the desert and camp under the stars. This serene location offers the perfect mix of solitude and adventure.',
+    rating: 4.6,
+    price: '150',
+    duration: '2',
+    location: 'Sahara Desert, Morocco',
+    category: 'Camping',
+  },
+  {
+    id: 18,
+    name: 'Forest Canopy Campsite',
+    image:
+      'https://images.pexels.com/photos/2898221/pexels-photo-2898221.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Camp among the treetops in this unique forest canopy campsite. Ideal for nature lovers who want a bird’s-eye view of the world below.',
+    rating: 4.9,
+    price: '175',
+    duration: '3',
+    location: 'Amazon Rainforest, Brazil',
+    category: 'Camping',
+  },
+  {
+    id: 19,
+    name: 'White Sand Beach Resort',
+    image:
+      'https://images.pexels.com/photos/240526/pexels-photo-240526.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Relax on the white sandy beaches of this tropical resort, with crystal-clear waters and beautiful sunsets.',
+    rating: 4.8,
+    price: '400',
+    duration: '5',
+    location: 'Maldives',
+    category: 'Beaches',
+  },
+  {
+    id: 20,
+    name: 'Golden Coast Beach Resort',
+    image:
+      'https://images.pexels.com/photos/3355788/pexels-photo-3355788.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Enjoy pristine golden beaches with luxury accommodations. Perfect for sunbathing, swimming, and water sports.',
+    rating: 4.7,
+    price: '350',
+    duration: '4',
+    location: 'Gold Coast, Australia',
+    category: 'Beaches',
+  },
+  {
+    id: 21,
+    name: 'Paradise Beach Escape',
+    image:
+      'https://images.pexels.com/photos/3355732/pexels-photo-3355732.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'A remote and secluded beach, perfect for those seeking an intimate and quiet getaway, with stunning views of the ocean.',
+    rating: 4.9,
+    price: '500',
+    duration: '7',
+    location: 'Bora Bora, French Polynesia',
+    category: 'Beaches',
+  },
+  {
+    id: 22,
+    name: 'City Museum Exploration',
+    image:
+      'https://images.pexels.com/photos/69903/pexels-photo-69903.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Discover ancient art and artifacts in this historic museum. Perfect for art lovers and history buffs.',
+    rating: 4.7,
+    price: '25',
+    duration: '2',
+    location: 'London, UK',
+    category: 'Museums',
+  },
+  {
+    id: 23,
+    name: 'Futuristic Art Gallery',
+    image:
+      'https://images.pexels.com/photos/1839919/pexels-photo-1839919.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Explore the future of art in this cutting-edge gallery featuring immersive installations and digital artworks.',
+    rating: 4.6,
+    price: '40',
+    duration: '1',
+    location: 'Tokyo, Japan',
+    category: 'Museums',
+  },
+  {
+    id: 24,
+    name: 'Museum of Natural History',
+    image:
+      'https://images.pexels.com/photos/24531675/pexels-photo-24531675/free-photo-of-close-up-of-valencia-landmarks-l-hemisferic-and-queen-sofia-palace-of-the-arts.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Discover the wonders of the natural world, from prehistoric fossils to ecosystems of the future.',
+    rating: 4.9,
+    price: '30',
+    duration: '3',
+    location: 'New York, USA',
+    category: 'Museums',
+  },
+  {
+    id: 25,
+    name: 'Historical City Tour',
+    image:
+      'https://images.pexels.com/photos/1191377/pexels-photo-1191377.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Step back in time with a guided tour through one of the most historic cities. See landmarks, ruins, and learn about ancient civilizations.',
+    rating: 4.7,
+    price: '100',
+    duration: '1',
+    location: 'Rome, Italy',
+    category: 'Tours',
+  },
+  {
+    id: 26,
+    name: 'Cultural Walking Tour',
+    image:
+      'https://images.pexels.com/photos/20006108/pexels-photo-20006108/free-photo-of-old-city.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Take a leisurely walk through the cultural heart of the city. Perfect for those wanting to learn more about the local traditions and history.',
+    rating: 4.4,
+    price: '50',
+    duration: '3',
+    location: 'Kyoto, Japan',
+    category: 'Tours',
+  },
+  {
+    id: 27,
+    name: 'Island Hopping Adventure',
+    image:
+      'https://images.pexels.com/photos/27705911/pexels-photo-27705911/free-photo-of-plynaca-lodz-na-tle-budynkow-mieszkalnym-rzeka-odra.jpeg?auto=compress&cs=tinysrgb&w=600',
+    description:
+      'Explore multiple stunning islands in one trip. Perfect for adventure seekers and beach lovers looking for a diverse experience.',
+    rating: 4.9,
+    price: '300',
+    duration: '4',
+    location: 'Greece',
+    category: 'Tours',
+  },
 ];
